6f956f335677cf21bfe089f8be830f9774e90e2e
Craig Langerman <craig.langerman@ni.com>
2022-04-27 21:14:42 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,6 @@
-module "s3_bucket" {
-  source = "app.terraform.io/bgunter/s3-bucket/aws"
-
-    bucket_prefix = var.prefix
-    acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/cdw-lab/s3-bucket/aws"
+  version = "2.8.0"
+  # insert required variables here
+  bucket_prefix = var.prefix
 }
